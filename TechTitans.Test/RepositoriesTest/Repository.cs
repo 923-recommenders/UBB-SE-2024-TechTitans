d89d115a0d8ee7dae33dbe74b16dafa5a97bc607
GeanovuMedeea <medeea.geanovu@gmail.com>
2024-04-20 03:14:40 +0300

    Test Folders and Csproj settings

---
+++ TechTitans.Test/RepositoriesTest/Repository.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TechTitans;
+
+namespace TechTitans.Test.Repositories
+{
+    internal class Repository
+    {
+        [SetUp]
+        public void Setup() { }
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TechTitans/Database/ApplicationDatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+
+
+namespace TechTitans.Database
+{
+    internal class ApplicationDatabaseContext
+    {
+        private readonly string _connectionString;
+
+        public ApplicationDatabaseContext()
+        {
+            _connectionString = $"Data Source=tcp:{Environment.GetEnvironmentVariable("DB_IP")},1433;User ID={Environment.GetEnvironmentVariable("DB_USER")};" +
+                                $"Password={Environment.GetEnvironmentVariable("DB_PASS")};Initial Catalog={Environment.GetEnvironmentVariable("DB_SCHEMA")};TrustServerCertificate=True"
+;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
