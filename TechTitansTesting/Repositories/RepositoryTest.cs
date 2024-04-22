377e07dfc5ea4949690d9168f62a640e72f11f6c
Olivia <olivia.fintina@yahoo.com>
2024-04-23 01:33:51 +0300

    Finished The Tests for the Repo, with all testing scenarios

--- TechTitansTesting/Repositories/RepositoryTest.cs
+++ TechTitansTesting/Repositories/RepositoryTest.cs
@@ -5,6 +5,10 @@ using Dapper;
 using Moq;
 using Xunit;
 using TechTitans.Repositories;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Xunit.Sdk;
 
 namespace TechTitansTesting.Repositories
 {
@@ -33,6 +37,20 @@ namespace TechTitansTesting.Repositories
         }
 
         [Fact]
+        public void Add_WhenDatabaseOperationsThrowsException_ShouldReturnFalse()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            var entity = new TestEntity { Id = 1, Name = "Test" };
+            _mockDatabaseOperations.Setup(c => c.Execute(It.IsAny<string>(), It.IsAny<TestEntity>(), null, null, null))
+                           .Throws(new Exception("Test exception"));
+
+            var result = _repository.Add(entity);
+
+            Assert.False(result);
+        }
+
+        [Fact]
         public void Delete_WhenEntityIsDeleted_ShouldReturnTrue()
         {
             _mockDatabaseOperations = new Mock<IDatabaseOperations>();
@@ -44,6 +62,20 @@ namespace TechTitansTesting.Repositories
             var result = _repository.Delete(entity);
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Delete_WhenDatabaseOperationsThrowsException_ShouldReturnFalse()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            var entity = new TestEntity { Id = 1, Name = "Test" };
+            _mockDatabaseOperations.Setup(c => c.Execute(It.IsAny<string>(), It.IsAny<TestEntity>(), null, null, null))
+                           .Throws(new Exception("Test exception"));
+
+            var result = _repository.Delete(entity);
+
+            Assert.False(result);
         }
 
         [Fact]
@@ -61,6 +93,32 @@ namespace TechTitansTesting.Repositories
         }
 
         [Fact]
+        public void GetAll_ShouldReturnEmptyList_WhenEntitiesDoNotExist()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            _mockDatabaseOperations.Setup(c => c.Query<TestEntity>(It.IsAny<string>(), null, null, true, null, null))
+                           .Returns(new List<TestEntity>());
+
+            var result = _repository.GetAll();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetAll_WhenDatabaseOperationsThrowsException_ShouldReturnNull()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            _mockDatabaseOperations.Setup(c => c.Query<TestEntity>(It.IsAny<string>(), null, null, true, null, null))
+                           .Throws(new Exception("Test exception"));
+
+            var result = _repository.GetAll();
+
+            Assert.Null(result); 
+        }
+
+        [Fact]
         public void GetById_WhenEntityExists_ShouldReturnEntity()
         {
             _mockDatabaseOperations = new Mock<IDatabaseOperations>();
@@ -74,11 +132,112 @@ namespace TechTitansTesting.Repositories
             Assert.NotNull(result);
             Assert.Equal(1, result.Id);
         }
+
+        [Fact]
+        public void GetById_WhenEntityDoesNotExist_ShouldReturnNull()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            _mockDatabaseOperations.Setup(c => c.Query<TestEntity>(It.IsAny<string>(), null, null, true, null, null))
+                           .Returns(new List<TestEntity>());
+
+            var result = _repository.GetById(1);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Update_WhenEntityIsUpdated_ShouldReturnTrue()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            var entity = new TestEntity { Id = 1, Name = "UpdatedTest" };
+
+            _mockDatabaseOperations.Setup(c => c.Execute(It.IsAny<string>(), It.IsAny<TestEntity>(), null, null, null))
+                                   .Returns(1);
+
+            
+            var result = _repository.Update(entity);
+
+            
+            Assert.True(result);
+            _mockDatabaseOperations.Verify(c => c.Execute(It.IsAny<string>(), It.IsAny<TestEntity>(), null, null, null), Times.Once);
+        }
+
+        [Fact]
+        public void Update_WhenDatabaseOperationsThrowsException_ShouldReturnFalse()
+        {
+            _mockDatabaseOperations = new Mock<IDatabaseOperations>();
+            _repository = new Repository<TestEntity>(_mockDatabaseOperations.Object);
+            var entity = new TestEntity { Id = 1, Name = "UpdatedTest" };
+            var expectedExceptionMessage = "Test exception";
+
+            _mockDatabaseOperations.Setup(c => c.Execute(It.IsAny<string>(), It.IsAny<TestEntity>(), null, null, null))
+                                   .Throws(new Exception("Test exception"));
+
+
+            bool result = _repository.Update(entity);
+            
+
+            Assert.False(result);
+          }
+
+
+        [Fact]
+        public void GetKeyColumnName_WhenColumnAttributeIsSpecified_ShouldReturnColumnName()
+        {
+
+            string expectedColumnName = "Id"; 
+
+
+            string actualColumnName = Repository<TestEntity>.GetKeyColumnName(); 
+
+            
+            Assert.Equal(expectedColumnName, actualColumnName);
+        }
+
+        [Fact]
+        public void GetKeyColumnName_WhenColumnAttributeIsNotSpecified_ShouldReturnProprietyName()
+        {
+            string expectedColumnName = "Id";
+
+            string actualColumnName = Repository<TestEntityWithoutColumnAttribute>.GetKeyColumnName();
+
+            Assert.Equal(expectedColumnName, actualColumnName);
+        }
+
+        [Fact]
+        public void GetKeyColumnName_WhenThereIsNoKey_ShouldReturnNull()
+        {
+            string actualColumnName = Repository<TestEntityWithoutKey>.GetKeyColumnName();
+
+            Assert.Null(actualColumnName);
+        }
+
     }
 
     public class TestEntity
     {
+        [Key]
+        [Column("Id")]
         public int Id { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+    }
+
+    public class TestEntityWithoutColumnAttribute
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class TestEntityWithoutKey
+    {
+        public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
