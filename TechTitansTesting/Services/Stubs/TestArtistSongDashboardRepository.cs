b8f33b9a80dfdbacd0e1f2ed1a9b9504634167ed
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-04-23 00:11:30 +0300

    Provided tests for the ArtistSongDashboardController class

---
+++ TechTitansTesting/Services/Stubs/TestArtistSongDashboardRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTitans.Repositories;
+
+namespace TechTitansTesting.Services.Stubs
+{
+    internal class TestArtistSongDashboardRepository<T> : IRepository<T> where T : class
+    {
+        private readonly List<T> _data;
+
+        public TestArtistSongDashboardRepository(List<T> data)
+        {
+            _data = data;
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _data;
+        }
+
+        public bool Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
